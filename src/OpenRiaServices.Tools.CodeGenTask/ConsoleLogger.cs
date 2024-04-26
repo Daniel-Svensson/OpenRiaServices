6f4c845f3701f51444378447a9d73250f2e2b504
Daniel Svensson <daniel.svensson@crm.se>
2024-04-26 12:02:31 +0200

    Test spectr.cli

--- src/OpenRiaServices.Tools.CodeGenTask/ConsoleLogger.cs
+++ src/OpenRiaServices.Tools.CodeGenTask/ConsoleLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Spectre.Console;
 
 namespace OpenRiaServices.Tools.CodeGenTask;
 
@@ -21,20 +22,7 @@ class ConsoleLogger : ILoggingService
 
     public void LogException(Exception ex)
     {
-        TextWriter writer = Console.Out;
-
-        writer.WriteLine("-----------------------------------------------------------------------------");
-        writer.WriteLine("Date : " + DateTime.Now.ToString());
-        writer.WriteLine();
-
-        while (ex != null)
-        {
-            writer.WriteLine(ex.GetType().FullName);
-            writer.WriteLine("Message : " + ex.Message);
-            writer.WriteLine("StackTrace : " + ex.StackTrace);
-
-            ex = ex.InnerException;
-        }
+        AnsiConsole.WriteException(ex);
     }
 
     public void LogMessage(string message)
--- src/OpenRiaServices.Tools.CodeGenTask/Program.cs
+++ src/OpenRiaServices.Tools.CodeGenTask/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.CommandLine;
 using System.Collections.Generic;
+using Spectre.Console.Cli;
 
 namespace OpenRiaServices.Tools.CodeGenTask;
 
 static class Program
 {
+
     static int Main(string[] args)
     {
         // TODO: Remove dependency on MSBuild and then remove any PackageReferences to MSBuild and MSBuildLocator
@@ -16,110 +17,147 @@ static class Program
         Microsoft.Build.Locator.MSBuildLocator.RegisterInstance(Microsoft.Build.Locator.MSBuildLocator.QueryVisualStudioInstances().OrderByDescending(
            instance => instance.Version).First());
 
-        // TODO: Look at which options should be required. Should be the same as in OpenRiaServices.Client.CodeGen.targets
-        var languageOption = new Option<string>("--language");
-        var clientFrameworkOption = new Option<string>("--clientFrameworkPath");
-        var serverProjectPathOption = new Option<string>("--serverProjectPath");
-        var clientProjectPathOption = new Option<string>("--clientProjectPath");
-        var clientRootNamespaceOption = new Option<string>("--clientRootNamespace");
-        var serverRootNamespaceOption = new Option<string>("--serverRootNamespace");
-        var isApplicationContextGenerationEnabledOption = new Option<bool>("--isApplicationContextGenerationEnabled");
-        var clientProjectTargetPlatformsOption = new Option<TargetPlatform>("--clientProjectTargetPlatform");
-        var useFullTypeNamesOption = new Option<bool>("--useFullTypeNames");
-
-        var sharedSourceFilesOption = new Option<IEnumerable<string>>("--sharedSourceFiles") { AllowMultipleArgumentsPerToken = true };
-        var symbolSearchPathsOption = new Option<IEnumerable<string>>("--symbolSearchPaths") { AllowMultipleArgumentsPerToken = true };
-        var serverAssembliesOption = new Option<IEnumerable<string>>("--serverAssemblies") { AllowMultipleArgumentsPerToken = true };
-        var clientAssembliesOption = new Option<IEnumerable<string>>("--clientAssemblies") { AllowMultipleArgumentsPerToken = true };
-        var clientAssemblyPathsNormalizedOption = new Option<IEnumerable<string>>("--clientAssemblyPathsNormalized") { AllowMultipleArgumentsPerToken = true };
-
-        var codeGeneratorName = new Option<string>(name: "--codeGeneratorName") { };
-
-        var generatedFileName = new Option<string>(name: "--generatedFileName")
+        if (args.Length == 1 && args[0].StartsWith('@'))
         {
-            IsRequired = true
-        };
+            args = File.ReadAllLines(args[0].Substring(1));
 
-        var loggingPipe = new Option<string>(name: "--loggingPipe");
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith('"') && args[i].EndsWith('"'))
+                    args[i] = args[i].Substring(1, args[i].Length - 2);
+            }
+        }
 
-        var rootCommand = new RootCommand("Sample app for running code generation")
-        {
-            languageOption,
-            clientFrameworkOption,
-            serverProjectPathOption,
-            clientProjectPathOption,
-            clientRootNamespaceOption,
-            serverRootNamespaceOption,
-            isApplicationContextGenerationEnabledOption,
-            clientProjectTargetPlatformsOption,
-            useFullTypeNamesOption,
-            sharedSourceFilesOption,
-            symbolSearchPathsOption,
-            serverAssembliesOption,
-            clientAssembliesOption,
-            clientAssemblyPathsNormalizedOption,
-            codeGeneratorName,
-            generatedFileName,
-            loggingPipe,
-        };
+        var app = new CommandApp<CodeGenerationCommand>();
+        return app.Run(args);
 
-        bool success = false;
-
-       rootCommand.SetHandler((clientCodeGenerationOptionValue, sharedCodeServiceParametersValue, codeGeneratorName, generatedFileName, pipeName)
-            => success = RunCodeGenForNet6(clientCodeGenerationOptionValue, sharedCodeServiceParametersValue, codeGeneratorName, generatedFileName, pipeName),
-            new ClientCodeGenerationOptionsBinder(
-                languageOption, clientFrameworkOption, serverProjectPathOption, clientProjectPathOption, clientRootNamespaceOption, serverRootNamespaceOption,
-                isApplicationContextGenerationEnabledOption, clientProjectTargetPlatformsOption, useFullTypeNamesOption),
-            new SharedCodeServiceParametersBinder(sharedSourceFilesOption, symbolSearchPathsOption, serverAssembliesOption, clientAssembliesOption, clientAssemblyPathsNormalizedOption),
-            codeGeneratorName,
-            generatedFileName,
-            loggingPipe);
-
-        rootCommand.Invoke(args);
-        if (success)
-            return 0;
-        else
-            return -1;
-    }
-
-    private static bool RunCodeGenForNet6(ClientCodeGenerationOptions clientCodeGenerationOption, SharedCodeServiceParameters sharedCodeServiceParameters, string codeGeneratorName, string generatedFileName, string loggingPipe)
-    {
-        ILoggingService log = string.IsNullOrEmpty(loggingPipe) ? new ConsoleLogger() : new OpenRiaServices.Tools.Logging.CrossProcessLoggingWriter(loggingPipe);
         
-        try
-        {
-            log.LogMessage($"OpenRiaServices CodeGen running on {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}");
-
-            SetupAppConfig(clientCodeGenerationOption);
-            RiaClientFilesTaskHelpers.CodeGenForNet6(generatedFileName, clientCodeGenerationOption, log, sharedCodeServiceParameters, codeGeneratorName);
-            log.LogMessage("Code generation succeeded");
-            return true;
-        }
-        catch (Exception ex)
-        {
-            log.LogException(ex);
-            return false;
-        }
-        finally
-        {
-            (log as IDisposable)?.Dispose();
-        }
     }
 
-    // Find app.config/web.config based on https://stackoverflow.com/questions/4738/using-configurationmanager-to-load-config-from-an-arbitrary-location/14246260#14246260
-    // Note: This just looks for "app.config" in the root, we might want to be smarter when searching for them.
-    // Note: Prefer web.config if running on NETFRAMEWORK
-    // Note we probably want to change this to a recursive search
-    // (using glob pattern to ignore bin/obj folders)
-    private static void SetupAppConfig(ClientCodeGenerationOptions clientCodeGenerationOption)
+    class CodeGenerationCommand : Command<CodeGenerationCommand.CodeGenerationSettings>
     {
-        var serverProjectPath = Path.GetDirectoryName(clientCodeGenerationOption.ServerProjectPath);
-
-        var configFiles = Directory.GetFiles(serverProjectPath, "*.config");
-        var configFile = configFiles.FirstOrDefault(f => f.EndsWith("app.config", StringComparison.InvariantCultureIgnoreCase));
-        if (configFile != null)
+        public class CodeGenerationSettings : CommandSettings
         {
-            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", configFile);
+            [CommandOption("--language <LANGUAGE>")]
+            public string Language { get; set; }
+
+            [CommandOption("--clientFrameworkPath <CLIENT_FRAMEWORK_PATH>")]
+            public string ClientFrameworkPath { get; set; }
+
+            [CommandOption("--serverProjectPath <SERVER_PROJECT_PATH>")]
+            public string ServerProjectPath { get; set; }
+            [CommandOption("--clientProjectPath <SERVER_PROJECT_PATH>")]
+            public string ClientProjectPath { get; set; }
+
+            [CommandOption("--clientRootNamespace")]
+            public string clientRootNamespace { get; set; }
+            [CommandOption("--serverRootNamespace")]
+            public string serverRootNamespace { get; set; }
+            [CommandOption("--isApplicationContextGenerationEnabled")]
+            public bool isApplicationContextGenerationEnabled { get; set; }
+            [CommandOption("--clientProjectTargetPlatform")]
+            public TargetPlatform clientProjectTargetPlatform { get; set; }
+            [CommandOption("--useFullTypeNames")]
+            public bool useFullTypeNames { get; set; }
+
+            [CommandOption("--codeGeneratorName")]
+            public string codeGeneratorName { get; set; }
+            [CommandOption("--generatedFileName")]
+            public string generatedFileName { get; set; }
+
+            [CommandOption("--sharedSourceFiles")]
+            public string[] sharedSourceFiles { get; set; }
+
+            [CommandOption("--symbolSearchPaths")]
+            public string[] symbolSearchPaths { get; set; }
+
+            [CommandOption("--serverAssemblies")]
+            public string[] serverAssemblies { get; set; }
+
+            [CommandOption("--clientAssemblies")]
+            public string[] clientAssemblies { get; set; }
+
+            [CommandOption("--clientAssemblyPathsNormalized")]
+            public string[] clientAssemblyPathsNormalized { get; set; }
+
+            [CommandOption("--loggingPipe")]
+            public string LoggingPipe { get; set; }
+
+            public ClientCodeGenerationOptions GetCodeGenerationOptions()
+            {
+                return new ClientCodeGenerationOptions
+                {
+                    Language = Language,
+                    ClientFrameworkPath = ClientFrameworkPath,
+                    ServerProjectPath = ServerProjectPath,
+                    ClientProjectPath = ClientProjectPath,
+                    ClientRootNamespace = clientRootNamespace,
+                    ServerRootNamespace = serverRootNamespace,
+                    IsApplicationContextGenerationEnabled = isApplicationContextGenerationEnabled,
+                    ClientProjectTargetPlatform = clientProjectTargetPlatform,
+                    UseFullTypeNames = useFullTypeNames,
+                };
+            }
+
+            public SharedCodeServiceParameters GetSharedCodeServiceParameters()
+            {
+                return new SharedCodeServiceParameters
+                {
+                    // TODO: should we use default names such as "--shared-source-files" 
+                    SharedSourceFiles = sharedSourceFiles,
+                    SymbolSearchPaths = symbolSearchPaths,
+                    ServerAssemblies = serverAssemblies,
+                    ClientAssemblies = clientAssemblies,
+                    ClientAssemblyPathsNormalized = clientAssemblyPathsNormalized,
+                };
+            }
+        }
+
+        public override int Execute(CommandContext context, CodeGenerationSettings settings)
+        {
+            ILoggingService log = string.IsNullOrEmpty(settings.LoggingPipe)
+                ? new ConsoleLogger() : new OpenRiaServices.Tools.Logging.CrossProcessLoggingWriter(settings.LoggingPipe);
+
+            try
+            {
+                log.LogMessage($"OpenRiaServices CodeGen running on {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}");
+
+                var clientCodeGenerationOptions = settings.GetCodeGenerationOptions();
+                var sharedServiceParameters = settings.GetSharedCodeServiceParameters();
+
+                SetupAppConfig(clientCodeGenerationOptions);
+                RiaClientFilesTaskHelpers.CodeGenForNet6(settings.generatedFileName, clientCodeGenerationOptions, log, sharedServiceParameters, settings.codeGeneratorName);
+                log.LogMessage("Code generation succeeded");
+
+                // Success
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                log.LogException(ex);
+                return -1;
+            }
+            finally
+            {
+                (log as IDisposable)?.Dispose();
+            }
+        }
+
+        // Find app.config/web.config based on https://stackoverflow.com/questions/4738/using-configurationmanager-to-load-config-from-an-arbitrary-location/14246260#14246260
+        // Note: This just looks for "app.config" in the root, we might want to be smarter when searching for them.
+        // Note: Prefer web.config if running on NETFRAMEWORK
+        // Note we probably want to change this to a recursive search
+        // (using glob pattern to ignore bin/obj folders)
+        private static void SetupAppConfig(ClientCodeGenerationOptions clientCodeGenerationOption)
+        {
+            var serverProjectPath = Path.GetDirectoryName(clientCodeGenerationOption.ServerProjectPath);
+
+            var configFiles = Directory.GetFiles(serverProjectPath, "*.config");
+            var configFile = configFiles.FirstOrDefault(f => f.EndsWith("app.config", StringComparison.InvariantCultureIgnoreCase));
+            if (configFile != null)
+            {
+                AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", configFile);
+            }
         }
     }
 }
